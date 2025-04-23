c189864e9d44227210ec0c6352803c005c4763aa
Juan Javier Pedernera <102430306+Javier-Pedernera@users.noreply.github.com>
2025-04-23 12:59:15 -0300

    cambio puerto a 5001

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 # run.py
 from app import create_app
+import logging
 
 app = create_app()
 
 if __name__ == "__main__":
+    
+    logging.basicConfig(level=logging.INFO)
     app.run(host="0.0.0.0", port=5001, debug=True)
